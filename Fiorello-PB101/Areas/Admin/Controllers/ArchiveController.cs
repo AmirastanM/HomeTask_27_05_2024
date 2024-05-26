2e13cffb7f0a77ae5fbc07cf76c232ac766d3777
AmirastanM <amirastanshm@code.edu.az>
2024-05-27 01:20:23 +0400

    hometask

---
+++ Fiorello-PB101/Areas/Admin/Controllers/ArchiveController.cs
@@ -0,0 +1,47 @@
+﻿using Fiorello_PB101.Data;
+using Fiorello_PB101.Helpers;
+using Fiorello_PB101.Services.Interfaces;
+using Fiorello_PB101.ViewModels.Categories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiorello_PB101.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArchiveController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly AppDbContext _context;
+        public ArchiveController(ICategoryService categoryService,
+                                  AppDbContext context)
+        {
+            _categoryService = categoryService;
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            var products = await _categoryService.GetAllPaginateAsync(page, 4);
+
+            var mappedDatas = _categoryService.GetMappedDatas(products);
+
+            int totalPage = await GetPageCountAsync(4);
+
+            Paginate<CategoryProductVM> paginateDatas = new(mappedDatas, totalPage, page);
+
+            return View(paginateDatas);
+        }
+          private async Task<int> GetPageCountAsync(int take)
+        {
+            int productCount = await _categoryService.GetCountAsync();
+
+            return (int)Math.Ceiling((decimal)productCount / take);
+        }
+
+        public async Task<IActionResult> CategoryArchive()
+        {
+            return View( await _categoryService.GetAllArchiveAsync());
+        }
+    }
+}
---
+++ Fiorello-PB101/Helpers/Paginate.cs
@@ -0,0 +1,45 @@
+﻿using Fiorello_PB101.ViewModels.Categories;
+
+namespace Fiorello_PB101.Helpers
+{
+    public class Paginate<T>
+    {
+        private IEnumerable<CategoryProductVM> mappedDatas;
+       
+        private int page;
+
+        public IEnumerable<T> Datas { get; set; }
+        public int TotalPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        public Paginate(IEnumerable<T> datas, int totalPage, int currentPage)
+        {
+            Datas = datas;
+            TotalPage = totalPage;
+            CurrentPage = currentPage;
+        }
+
+        public Paginate(IEnumerable<CategoryProductVM> mappedDatas, int totalPage, int page)
+        {
+            this.mappedDatas = mappedDatas;
+            TotalPage = totalPage;
+            this.page = page;
+        }
+
+        public bool HasNext
+        {
+            get
+            {
+                return CurrentPage < TotalPage;
+            }
+        }
+
+        public bool HasPrevious
+        {
+            get
+            {
+                return CurrentPage > 1;
+            }
+        }
+    }
+}
